f70c04eb5986a8ede827d1d04eda1e2938fdb825
Olia <oliaopanasiukk@gmail.com>
2025-11-16 17:14:47 +0200

    lab 4(2)

--- flutter_init/lib/main.dart
+++ flutter_init/lib/main.dart
@@ -1,90 +1,70 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(const HeroApp());
-
-class HeroApp extends StatelessWidget {
-  const HeroApp({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Hero Animation Demo',
-      home: MainScreen(),
-    );
-  }
+void main() {
+  runApp(const MaterialApp(
+    debugShowCheckedModeBanner: false,
+    title: 'Navigation Basics',
+    home: FirstRoute(),
+  ));
 }
 
-class MainScreen extends StatelessWidget {
-  const MainScreen({Key? key}) : super(key: key);
+class FirstRoute extends StatelessWidget {
+  const FirstRoute({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text(
-          'Main Screen',
+          'First Route',
           style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
         ),
         centerTitle: true,
-        backgroundColor: const Color.fromARGB(130, 121, 23, 72),
+        backgroundColor: const Color.fromARGB(155, 33, 149, 243),
       ),
-      body: GestureDetector(
-        onTap: () {
-          Navigator.push(context, MaterialPageRoute(builder: (context) {
-            return const DetailScreen();
-          }));
-        },
-        child: Center(
-          child: Hero(
-            tag: 'imageHero',
-            child: ClipRRect(
-              borderRadius: BorderRadius.circular(16),
-              child: Image.network(
-                'https://picsum.photos/250?image=9',
-                width: 250,
-                height: 250,
-                fit: BoxFit.cover,
-              ),
-            ),
+      body: Center(
+        child: ElevatedButton(
+          style: ElevatedButton.styleFrom(
+            padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 12),
+            textStyle: const TextStyle(fontSize: 18),
           ),
+          child: const Text('Open Second Route'),
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => const SecondRoute()),
+            );
+          },
         ),
       ),
     );
   }
 }
 
-class DetailScreen extends StatelessWidget {
-  const DetailScreen({Key? key}) : super(key: key);
+class SecondRoute extends StatelessWidget {
+  const SecondRoute({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text(
-          'Detail Screen',
+          "Second Route",
           style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
         ),
         centerTitle: true,
-        backgroundColor: const Color.fromARGB(87, 97, 19, 121),
+        backgroundColor: const Color.fromARGB(155, 33, 149, 243),
       ),
-      body: GestureDetector(
-        onTap: () {
-          Navigator.pop(context);
-        },
-        child: Center(
-          child: Hero(
-            tag: 'imageHero',
-            child: ClipRRect(
-              borderRadius: BorderRadius.circular(24),
-              child: Image.network(
-                'https://picsum.photos/250?image=9',
-                width: 350,
-                height: 350,
-                fit: BoxFit.cover,
-              ),
-            ),
+      body: Center(
+        child: ElevatedButton(
+          style: ElevatedButton.styleFrom(
+            padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 12),
+            textStyle: const TextStyle(fontSize: 18),
           ),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          child: const Text('Go Back!'),
         ),
       ),
     );
