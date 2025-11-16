b3f913821197d85e348bf91b3bf51c581822a275
Olia <oliaopanasiukk@gmail.com>
2025-11-16 17:11:17 +0200

    lab 4(1)

--- flutter_init/lib/main.dart
+++ flutter_init/lib/main.dart
@@ -1,79 +1,92 @@
 import 'package:flutter/material.dart';
-import 'package:fluttertoast/fluttertoast.dart';
 
-final List<String> pizza = <String>["Маргарита", "М'ясна", "Чотири сири"];
+void main() => runApp(const HeroApp());
 
-final List<String> receptPizza = <String>[
-  "Кукурудза, томати, сир, курка, ананас",
-  "Бекон, мисливські ковбаски, сир",
-  "Чедер, Мацарела, Фетта, Рокфорд",
-];
+class HeroApp extends StatelessWidget {
+  const HeroApp({Key? key}) : super(key: key);
 
-void main() {
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Pizza App',
-      theme: ThemeData(primarySwatch: Colors.red),
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text(
-            "Список піц",
-            style: TextStyle(
-              fontFamily: "Nunito",
-              fontSize: 26,
-              fontWeight: FontWeight.w700,
-              color: const Color.fromARGB(227, 66, 31, 91),
-              letterSpacing: 1.2,
+      title: 'Hero Animation Demo',
+      home: MainScreen(),
+    );
+  }
+}
+
+class MainScreen extends StatelessWidget {
+  const MainScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Main Screen',
+          style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+        ),
+        centerTitle: true,
+        backgroundColor: const Color.fromARGB(130, 121, 23, 72),
+      ),
+      body: GestureDetector(
+        onTap: () {
+          Navigator.push(context, MaterialPageRoute(builder: (context) {
+            return const DetailScreen();
+          }));
+        },
+        child: Center(
+          child: Hero(
+            tag: 'imageHero',
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(16),
+              child: Image.network(
+                'https://picsum.photos/250?image=9',
+                width: 250,
+                height: 250,
+                fit: BoxFit.cover,
+              ),
             ),
           ),
-          centerTitle: true,
         ),
-        body: PizzaListView(),
       ),
     );
   }
 }
 
-class PizzaListView extends StatelessWidget {
+class DetailScreen extends StatelessWidget {
+  const DetailScreen({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return ListView.separated(
-      padding: const EdgeInsets.all(8),
-      itemCount: pizza.length,
-      separatorBuilder: (BuildContext context, int index) => Divider(),
-      itemBuilder: (BuildContext context, int index) {
-        return ListTile(
-          leading: Icon(Icons.local_pizza, size: 28, color: Colors.red),
-          title: Text(
-            pizza[index],
-            style: TextStyle(
-              fontSize: 22,
-              fontWeight: FontWeight.bold,
-              fontFamily: "Nunito",
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Detail Screen',
+          style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+        ),
+        centerTitle: true,
+        backgroundColor: const Color.fromARGB(87, 97, 19, 121),
+      ),
+      body: GestureDetector(
+        onTap: () {
+          Navigator.pop(context);
+        },
+        child: Center(
+          child: Hero(
+            tag: 'imageHero',
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(24),
+              child: Image.network(
+                'https://picsum.photos/250?image=9',
+                width: 350,
+                height: 350,
+                fit: BoxFit.cover,
+              ),
             ),
           ),
-          subtitle: Text("Склад: ${receptPizza[index]}"),
-          trailing: Icon(Icons.arrow_forward_ios),
-          onTap: () {
-            print("Вибрано піцу: ${pizza[index]}");
-
-            Fluttertoast.showToast(
-              msg: "Вибрано піцу: ${pizza[index]}",
-              gravity: ToastGravity.CENTER,
-              timeInSecForIosWeb: 1,
-              backgroundColor: Colors.red,
-              textColor: Colors.white,
-              fontSize: 16.0,
-            );
-          },
-        );
-      },
+        ),
+      ),
     );
   }
 }
